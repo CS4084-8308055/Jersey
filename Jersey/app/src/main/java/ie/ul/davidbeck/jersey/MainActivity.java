6d8f2e0cfa0c70ad7526cc8bd470a54bfa42a0c9
David-Beck-UL <39083800+David-Beck-UL@users.noreply.github.com>
2018-10-23 21:28:18 +0100

    Reset and Settings added

--- Jersey/app/src/main/java/ie/ul/davidbeck/jersey/MainActivity.java
+++ Jersey/app/src/main/java/ie/ul/davidbeck/jersey/MainActivity.java
@@ -2,8 +2,10 @@ package ie.ul.davidbeck.jersey;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -79,14 +81,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_reset:
+                mCurrentJersey = new Jersey(getString(R.string.name_default),
+                                            Integer.parseInt(getString(R.string.number_default)),
+                                            Boolean.parseBoolean(getString(R.string.home_default)));
+                showCurrentJersey();
+                return true;
+            case R.id.action_settings:
+                startActivity(new Intent(Settings.ACTION_SETTINGS));
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
